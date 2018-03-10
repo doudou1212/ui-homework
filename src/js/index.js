a151ffab266f5913c8ec8f0390ded6a1b321cb16
jing sun <jing.sun@rea-group.com>
2018-03-10 11:25:58 +0800

    add url-loader to load font-icon resources

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,2 @@
 import * as A from '../styles/main.scss'
+import * as B from '../../resource/font-icons/style.css'
--- webpack/webpack-base.config.js
+++ webpack/webpack-base.config.js
@@ -25,6 +25,10 @@ module.exports = function() {
               }]
           })
         },
+        {
+          test: /\.(eot|svg|ttf|woff(2)?)(\?v=\d+\.\d+\.\d+)?/,
+          loader: 'url-loader'
+        }
       ]
     },
     plugins: [
